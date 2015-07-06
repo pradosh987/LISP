adb29e67e2b3ec9aad5b5a45f12df4cbdb8833a3
pradosh987 <pradosh987@gmail.com>
2015-07-06 15:30:11 +0530

    1.46

---
+++ 2A/1.46.scm
@@ -0,0 +1,38 @@
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (iterative-improve improve good-enough?)
+  (lambda (x)
+    (define (iterate improve guess)
+      (if(good-enough? guess)
+          guess
+          (iterate improve (improve guess))))
+    (iterate improve (improve x))))
+
+(define (sqr x g)
+  (define (improve guess)
+  (average guess (/ x guess)))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.0001))
+  ((iterative-improve improve good-enough?) (improve g)))
+
+(define (fixed-point f g)
+  (define tolrance 0.00001)
+  (define (improve guess)
+  	(f guess))
+  (define (good-enough? guess)
+    (< (abs (- (f guess) guess)) tolrance))
+  ((iterative-improve improve good-enough? ) (improve g)))
+
+;(fixed-point cos 1.0)
+;.7390893414033927
+
+(fixed-point (lambda (x) (+ (sin x) (cos x))) 1.0)
+;1.2587228743052672
+
+;(sqr 137 1.0)
+;11.704699917758145
+
+
+
+
